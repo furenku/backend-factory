ffb95cb72e7c0abe760ae0bc151fd5809e5db3e8
rodrigo-frenk <furenku@gmail.com>
2016-08-26 19:23:48 -0500

    bugfix: plugin now renders metaboxes

--- index.php
+++ index.php
@@ -5,9 +5,15 @@ Author: kernspaltung!
 Description: Backend Configuration tool for Developers
 */
 
-global $cpts;
+global $cpts, $metaboxes;
 
 include 'metaboxes/dynamic_metaboxes.php';
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+   die;
+}
+
 
 class BackendFactory {
 
@@ -34,7 +40,22 @@ class BackendFactory {
          $singular = $cpt['singular'];
          $plural = $cpt['plural'];
 
-         $labels = $this -> generate_labels( $singular, $plural );
+         $labels = array(
+      		'name'               => _x( $plural, 'post type general name', 'backend-factory' ),
+      		'singular_name'      => _x( $singular, 'post type singular name', 'backend-factory' ),
+      		'menu_name'          => _x( $plural, 'admin menu', 'backend-factory' ),
+      		'name_admin_bar'     => _x( $singular, 'add new on admin bar', 'backend-factory' ),
+      		'add_new'            => _x( 'Add New', $singular, 'backend-factory' ),
+      		'add_new_item'       => __( 'Add New ' . $singular, 'backend-factory' ),
+      		'new_item'           => __( 'New ' . $singular, 'backend-factory' ),
+      		'edit_item'          => __( 'Edit ' . $singular, 'backend-factory' ),
+      		'view_item'          => __( 'View ' . $singular, 'backend-factory' ),
+      		'all_items'          => __( 'All ' . $plural, 'backend-factory' ),
+      		'search_items'       => __( 'Search ' . $plural, 'backend-factory' ),
+      		'parent_item_colon'  => __( 'Parent ' . $plural . ':', 'backend-factory' ),
+      		'not_found'          => __( 'No ' . $plural . ' found.', 'backend-factory' ),
+      		'not_found_in_trash' => __( 'No ' . $plural . ' found in Trash.', 'backend-factory' )
+      	);
 
          $args = array(
       		'labels'             => $labels,
@@ -58,32 +79,45 @@ class BackendFactory {
 
    }
 
-
-   function generate_labels( $singular, $plural ) {
-
-      $labels = array(
-   		'name'               => _x( $plural, 'post type general name', 'backend-factory' ),
-   		'singular_name'      => _x( $singular, 'post type singular name', 'backend-factory' ),
-   		'menu_name'          => _x( $plural, 'admin menu', 'backend-factory' ),
-   		'name_admin_bar'     => _x( $singular, 'add new on admin bar', 'backend-factory' ),
-   		'add_new'            => _x( 'Add New', $singular, 'backend-factory' ),
-   		'add_new_item'       => __( 'Add New ' . $singular, 'backend-factory' ),
-   		'new_item'           => __( 'New ' . $singular, 'backend-factory' ),
-   		'edit_item'          => __( 'Edit ' . $singular, 'backend-factory' ),
-   		'view_item'          => __( 'View ' . $singular, 'backend-factory' ),
-   		'all_items'          => __( 'All ' . $plural, 'backend-factory' ),
-   		'search_items'       => __( 'Search ' . $plural, 'backend-factory' ),
-   		'parent_item_colon'  => __( 'Parent ' . $plural . ':', 'backend-factory' ),
-   		'not_found'          => __( 'No ' . $plural . ' found.', 'backend-factory' ),
-   		'not_found_in_trash' => __( 'No ' . $plural . ' found in Trash.', 'backend-factory' )
-   	);
-
-      return $labels;
-
-   }
-
 }
 
+
+
+class DynamicMetaboxes {
+
+   var $metaboxes;
+   public function init() {
+      $this->metaboxes = array();
+      add_action("add_meta_boxes", array( $this, 'register_metaboxes' ) );
+      // echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum praesentium odit sunt adipisci sequi, eaque tenetur quae libero!";
+   }
+   function add_metabox( $metabox = NULL ) {
+      if( $metabox ) {
+          array_push( $this -> metaboxes, $metabox );
+      }
+   }
+
+   public function register_metaboxes()
+   {
+      foreach($this -> metaboxes as $metabox) :
+         add_meta_box(
+            $metabox['post_type']."-meta-box",
+            $metabox['title'],
+            "standard_metabox_markup",
+            $metabox['post_type'],
+            "side",
+            "default",
+            array('metabox'=>$metabox)
+         );
+      endforeach;
+   }
+
+
+   public function testfunc() {
+echo '<h1>Init metaboxes</h1>';
+      var_dump("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit ab dolores magnam.");
+   }
+}
 
 add_action( 'init', 'backend_factory_init' );
 
@@ -93,14 +127,33 @@ function backend_factory_init() {
 
    $backendFactory->init();
 
+   $dynamic_metaboxes = new DynamicMetaboxes;
+
+      $dynamic_metaboxes -> init();
+
+
    include_once 'tests/bandas-personas.php';
+   include_once 'tests/metaboxes.php';
 
    foreach( $cpts as $cpt ) {
       $backendFactory -> add_cpt( $cpt );
    }
+   // add_action("add_meta_boxes", "add_dynamic_metaboxes");
+   foreach( $metaboxes as $metabox ) {
+      $dynamic_metaboxes -> add_metabox( $metabox );
+   }
+   // var_dump($backendFactory->metaboxes);
 
    $backendFactory -> register_cpts();
 
+   // add_action("add_metaboxes", array( $backendFactory, 'register_metaboxes' ), 1 );
+   add_action("add_metaboxes", array( $backendFactory, 'testfunc' ) );
+
+
+   // $backendFactory -> register_metaboxes();
+   // $backendFactory -> init_metaboxes();
+
+   // add_action( 'admin_init', array( $backendFactory, 'init_metaboxes' ) );
 }
 
 
--- metaboxes/metabox_markup.php
+++ metaboxes/metabox_markup.php
@@ -6,7 +6,7 @@ function standard_metabox_markup( $post,  $callback_args ) {
    $metabox = $args['metabox'];
 
    wp_nonce_field(basename(__FILE__), $metabox['name']."-metabox-nonce");
-   var_dump( get_post_meta($post->ID) );
+
    ?>
 
    <p>
@@ -18,94 +18,104 @@ function standard_metabox_markup( $post,  $callback_args ) {
 
       foreach($metabox['fields'] as $field ) :
 
+         // $field_name = $field['field_type'] . '-' . $metabox['post_type'];
+         $field_name = $metabox['post_type'];
+
          echo '<div class="field">';
 
-            echo '<div class="field_label">';
 
-            echo '<label for="'. $field['field_name'].'">'. $field['field_label'] .'</label>';
-
-            echo '</div>';
-
-
-            echo '<div class="field_inputs">';
+         // echo '<div class="field_label">';
+         //
+         // echo '<label for="'. $field['field_name'].'">'. $field['field_label'] .'</label>';
+         //
+         // echo '</div>';
 
 
+         echo '<div class="field_inputs">';
 
 
-            if( $field['field_type'] == "related_post" ) {
 
-               $related_posts = get_post_meta(
-               $post->ID,
-               $field['field_name'],
-               true
-            );
+
+         if( $field['field_type'] == "related_post" ) {
 
             foreach ($field['related_post_types'] as $related_post_type ) {
 
+               $related_post_type_field_name =  $field_name . '-' . $related_post_type;
+
                $posts = get_posts( array( 'post_type'=>$related_post_type ) );
 
-               related_post_selector( $field['field_name'] . '[]', $posts, 0, true );
+               related_post_selector( $related_post_type_field_name, $posts, 0, true );
 
-               if(is_array($related_posts)) {
-                  foreach( $related_posts as $related_post ) {
-                     if( $related_post != "0" )
-                     related_post_selector( $field['field_name'] . '[]', $posts, $related_post );
-                  }
+               $related_posts = get_post_meta(
+               $post->ID,
+               $related_post_type_field_name,
+               true
+            );
+
+            if(is_array($related_posts)) {
+               foreach( $related_posts as $related_post ) {
+                  if( $related_post != "0" )
+                  related_post_selector( $related_post_type_field_name, $posts, $related_post );
                }
-
-               related_post_selector( $field['field_name'] . '[]', $posts, 0 );
-
             }
 
+            related_post_selector( $related_post_type_field_name, $posts );
+
          }
-         if( $field['field_type'] == "datebooking" ) {
 
-            ?>
-            <div class="columns">
-               <?php
-               $fechas = get_post_meta( $post_id, 'dates', true );
-               var_dump( $fechas );
-
-               ?>
-            </div>
-            <div class="repeatable columns">
-
-               <div>
-                  <label for="start_date">
-                     Fecha de Inicio
-                  </label>
-                  <input type="text" name="start_date" class="start_date columns date start" />
-               </div>
+      }
 
 
-               <div>
-                  <label for="end_date">
-                     Fecha de Final
-                  </label>
-                  <input type="text" name="end_date" class="end_date columns date end" />
-               </div>
+      if( $field['field_type'] == "datebooking" ) {
 
+         ?>
 
-               <div>
-                  <label for="schedule">
-                     Horarios
-                  </label>
-                  <input type="text" name="schedule" class="schedule columns" value="">
-               </div>
-            </div>
+         <div class="columns">
             <?php
 
-         }
+            $fechas = get_post_meta( $post_id, 'dates', true );
+            var_dump( $fechas );
 
-         if( $field['repeatable'] ) {
             ?>
+         </div>
+         <div class="repeatable columns">
 
-            <div class="add_repeatable button">Añadir otro</div>
+            <div>
+               <label for="start_date">
+                  Fecha de Inicio
+               </label>
+               <input type="text" name="start_date" class="start_date columns date start" />
+            </div>
 
-            <?php
-         }
 
-         echo '</div>';
+            <div>
+               <label for="end_date">
+                  Fecha de Final
+               </label>
+               <input type="text" name="end_date" class="end_date columns date end" />
+            </div>
+
+
+            <div>
+               <label for="schedule">
+                  Horarios
+               </label>
+               <input type="text" name="schedule" class="schedule columns" value="">
+            </div>
+         </div>
+         <?php
+
+      }
+
+      if( $field['repeatable'] ) {
+         ?>
+
+         <div class="add_repeatable button">Añadir otro</div>
+
+         <?php
+      }
+
+      echo '</div>';
 
       echo '</div>';
 
@@ -132,29 +142,10 @@ jQuery(document).ready(function($){
 }
 
 
-
-
-add_action("add_meta_boxes", "add_dynamic_metaboxes");
-function add_dynamic_metaboxes()
-{
-   global $metaboxes;
-   foreach($metaboxes as $metabox) :
-      add_meta_box(
-      $metabox['post_type']."-meta-box",
-      $metabox['title'],
-      "standard_metabox_markup",
-      $metabox['post_type'],
-      "side",
-      "default",
-      array('metabox'=>$metabox)
-   );
-endforeach;
-
-
-}
-
-
 function related_post_selector( $name, $posts, $id=0, $hidden=false ) {
+
+   $name .= $name  . '[]';
+
    ?>
    <div class="repeatable <?php echo $hidden ? 'hidden':''; ?>">
 
