b46496de671a59c813285c1a4fa429e340fad4b7
furenku <furenku@gmail.com>
2016-11-09 22:32:19 -0600

    missing fields added.

--- assets/js/backend-factory.js
+++ assets/js/backend-factory.js
@@ -2,17 +2,7 @@ $ = jQuery.noConflict();
 
 $(document).ready(function(){
 
-   var datepicker = $('input.datepicker');
-
-   datepicker.each(function(){
-      console.log( $(this).data('target') );
-      $(this).datepicker({
-         altField: '#' + $(this).data('target'),
-         altFormat: "yy-mm-dd"
-      });
-
-   })
-
+   setup_inputs();
    setup_repeatables();
 
    console.log("Backend Factory: ready");
@@ -23,9 +13,9 @@ $(document).ready(function(){
 function setup_repeatables() {
 
    $('.add_repeatable').click(function(){
-      console.log( $(this).parent().attr('class') );
       var model = $(this).parent().find('.repeatable-model .input-container').clone().detach();
       $(this).parent().find('.field-repeatable-inputs').append( model );
+      setup_inputs();
       return false;
    })
 
@@ -45,6 +35,17 @@ function setup_repeatables() {
    if($('.repeatable-container:not(.hidden)').length>1) {
       $('.repeatable-container .delete_this').removeClass('disabled')
       $('.repeatable-container').each(function(){
+         var datepicker = $('input.datepicker');
+
+         datepicker.each(function(){
+            console.log( $(this).data('target') );
+            $(this).datepicker({
+               altField: '#' + $(this).data('target'),
+               altFormat: "yy-mm-dd"
+            });
+
+         })
+
          if( $(this).find('input').val().length == 0 ) {
             $(this).find('.delete_this').addClass('disabled')
          }
@@ -55,12 +56,57 @@ function setup_repeatables() {
 
    $('.repeatable-container input').keyup(function(){
       if( $(this).val().length != 0 ) {
-console.log("test!!");
          $(this).parent().find('.delete_this').removeClass('disabled');
       } else {
-console.log("test!!!!");
          $(this).parent().find('.delete_this').addClass('disabled');
       }
    })
 
+}
+
+
+
+
+
+function setup_inputs() {
+   setup_datepicker();
+   setup_uploader();
+}
+
+function setup_datepicker() {
+
+   var datepicker = $('input.datepicker');
+
+   datepicker.each(function(){
+      console.log( $(this).data('target') );
+      $(this).datepicker({
+         altField: '#' + $(this).data('target'),
+         altFormat: "yy-mm-dd"
+      });
+
+   })
+
+}
+
+
+function setup_uploader() {
+   $('.upload-button').click(function(e) {
+      e.preventDefault();
+
+      var button = $(this);
+
+      var file = wp.media({
+         title: 'Upload Image',
+         // mutiple: true if you want to upload multiple files at once
+         multiple: false
+      }).open()
+      .on('select', function(e){
+
+         var uploaded_file = file.state().get('selection').first();
+
+         var file_url = uploaded_file.toJSON().url;
+
+         button.parent().find('.upload_input').val(file_url);
+      });
+   });
 }
